8d28c29aacb09459b1efbebf7eb529526601442d
jayjay2397 <jj.valdez09@gmail.com>
2021-12-02 19:25:09 -0600

    adding more components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
+import Announcements from '../components/Announcements';
+import Slider from '../components/Slider';
+
 
 const Home = () => {
     return (
         <div>
+            <Announcements/>
             <Navbar/>
+            <Slider/>
         </div>
     )
 }
